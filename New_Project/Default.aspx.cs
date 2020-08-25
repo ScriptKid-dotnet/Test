747947719fdaace51775ae58a8f4c825a38ce4ce
Malte Weber <malte.weber@PCSMGmbH.onmicrosoft.com>
2020-08-25 11:19:59 +0200

    test2

--- New_Project/Default.aspx.cs
+++ New_Project/Default.aspx.cs
@@ -15,6 +15,10 @@ public partial class Default : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         int a = 1;
-        if (a == 1){}
+        int b = 2;
+        if (a == 1)
+        {
+            b++;
+        }
     }
 }
