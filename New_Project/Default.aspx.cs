de5e7d2d69d18f9b285a35930e071b4fef26b662
Malte Weber <malte.weber@PCSMGmbH.onmicrosoft.com>
2020-08-25 11:14:03 +0200

    ewwe

--- New_Project/Default.aspx.cs
+++ New_Project/Default.aspx.cs
@@ -14,7 +14,7 @@ public partial class Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        int a;
+        int a = 1;
         if (a == 1){}
     }
 }
